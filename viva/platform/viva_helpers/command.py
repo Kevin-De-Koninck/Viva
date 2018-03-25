5a6174de6f2d0d3438fc7d18df52f1b765268937
Kevin De Koninck <kevin.de.koninck92@gmail.com>
2018-03-25 16:32:50 +0200

    WAdded function to execute a shell command

--- viva/platform/viva_helpers/command.py
+++ viva/platform/viva_helpers/command.py
@@ -1,6 +1,22 @@
-from constants import Constants
+import subprocess
 
 class Command:
   SUCCESS = 0
   FAILED = 1
   UNKNOWN_COMMAND = -1
+
+
+  # usage: excecute("ls", "-l")
+  @staticmethod
+  def execute(self, *command):
+    try:
+      # Get stdout and stderr
+      output = subprocess.check_output(list(command), stderr=subprocess.STDOUT)
+      rc = 0
+
+    # If the executed command returns a nonzero exit code, an exception is raised
+    except subprocess.CalledProcessError as e:
+      output = e.output  # Output generated before error
+      rc = e.returncode  # Return code
+
+    return rc, output
