34f8c167d90af682969329b82e295d2a6655b663
Kevin De Koninck <kevin.de.koninck92@gmail.com>
2018-03-28 23:52:35 +0200

    Implement 'update packages' and 'install LEMP'

--- viva/platform/viva_helpers/constants.py
+++ viva/platform/viva_helpers/constants.py
@@ -5,7 +5,13 @@ class Constants:
     GROUP = "viva"
     USER = "viva"
 
+  class LEMP:
+    MYSQL_ROOT_PWD = "VivaMySQL"
+
+
   class Paths:
+    OPT_DIR = "/opt/viva"
+    BASH_SCRIPTS_DIR = os.path.join(OPT_DIR, "bash_scripts")
     LOG_DIR = "/var/log/viva"
     LOG_TERRARIUM = os.path.join(LOG_DIR, "terrarium.log")
     LOG_REPTILE = os.path.join(LOG_DIR, "reptile.log")
--- viva/platform/viva_helpers/system.py
+++ viva/platform/viva_helpers/system.py
@@ -1,17 +1,83 @@
+import os
+from constants import Constants
 from logger import Logger
 from command import Command
-from config_files import Unattended_upgrades
+from config_files import Unattended_upgrades, NginX
 
 
 class System(Command):
 
   def install_or_update_all(self):
-    return self.SUCCESS
+    rc = self.SUCCESS
+    rc |= self.enable_automatic_upgrades()
+    rc |= self.update_packages()
+    rc |= self.install_or_update_LEMP()
+    return rc
 
-  def install_or_update_nginx(self):
-    return self.SUCCESS
+  def install_or_update_LEMP(self, args):
+    # Set the root password for unattended install of MySQL
+    os.system('echo "mysql-server mysql-server/root_password password %s" | debconf-set-selections' % str(Constants.LEMP.MYSQL_ROOT_PWD))
+    os.system('echo "mysql-server mysql-server/root_password_again password %s" | debconf-set-selections' % str(Constants.LEMP.MYSQL_ROOT_PWD))
 
-  def install_or_update_mysql(self):
+    # Install MySQL
+    if self.execute("apt install -y mysql-server php-mysql libmysqlclient-dev").failed:
+      return self.FAILED
+    self.log(Logger.SUCCESS, "Successfully installed/updated MySQL.")
+
+    # Install NginX
+    for command in ["apt install -y nginx", "service nginx start"]:
+      if self.execute(command).failed:
+        return self.FAILED
+    self.log(Logger.SUCCESS, "Successfully installed/updated NginX.")
+
+    # Install PHP
+    if self.execute("apt install -y php-fpm").failed:
+      return self.FAILED
+    self.log(Logger.SUCCESS, "Successfully installed/updated PHP.")
+
+    # Configure NginX
+    if self.execute("mv %(file)s %(file)s.backup" % {'file': NginX.sites_available_default.filename}).failed:
+      return self.FAILED
+
+    try:
+      with open(NginX.sites_available_default.filename, "w+") as f:
+        f.write(NginX.sites_available_default.content)
+    except Exception as e:
+      self.log(Logger.ERROR, "An error occurred while writing to '%s': %s" % (NginX.sites_available_default.filename, str(e)))
+      return self.FAILED
+
+    if self.execute("adduser %s www-data" % Constants.Viva.USER).failed:
+      return self.FAILED
+    if self.execute("chown %s:www-data -R /usr/share/nginx/" % Constants.Viva.USER).failed:
+      return self.FAILED
+
+    self.log(Logger.SUCCESS, "Successfully configured NginX.")
+
+    # Install PHPMyAdmin
+    for command in ['echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections',
+                    'echo "phpmyadmin phpmyadmin/app-password-confirm password %s" | debconf-set-selections' % str(Constants.LEMP.MYSQL_ROOT_PWD),
+                    'echo "phpmyadmin phpmyadmin/mysql/admin-pass password %s" | debconf-set-selections' % str(Constants.LEMP.MYSQL_ROOT_PWD),
+                    'echo "phpmyadmin phpmyadmin/mysql/app-pass password %s" | debconf-set-selections' % str(Constants.LEMP.MYSQL_ROOT_PWD),
+                    'echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect" | debconf-set-selections']:
+      os.system(command)
+
+    for command in ["sudo apt install -y phpmyadmin", "service nginx reload", "service php7.0-fpm restart"]:
+      if self.execute(command).failed:
+        return self.FAILED
+
+    self.log(Logger.SUCCESS, "Successfully installed/configured PHPMyAdmin.")
+
+    # set an info page
+    os.system('echo "<?php phpinfo(); ?>" > /usr/share/nginx/html/info.php')
+
+    # DONE
+    self.log(Logger.SUCCESS, "Successfully installed/updated LEMP (PHPMyAdmin on port %d)." % NginX.sites_available_default.PHPMyAdmin_port)
+
+
+
+
+
+
     return self.SUCCESS
 
   def install_or_update_viva_framework(self):
@@ -20,13 +86,24 @@ class System(Command):
   def install_or_update_viva_website(self):
     return self.SUCCESS
 
-  def system_update(self):
-    return self.SUCCESS
+  def update_packages(self):
+    rc = self.SUCCESS
 
-  def automatic_upgrades(self):
-    rc, output = self.execute("apt", "install", "-y" ,"unattended-upgrades")
-    if rc:
-      self.log(Logger.ERROR, output)
+    for command in ["apt update", "apt upgrade -y", "apt dist-upgrade -y", "apt autoclean", "apt autoremove -y"]:
+      if self.execute(command).failed:
+        rc |=  self.FAILED
+    if rc == self.SUCCESS:
+      self.log(Logger.SUCCESS, "Successfully updated the OS packages.")
+
+    if self.execute(os.path.join(Constants.Paths.BASH_SCRIPTS_DIR, "pip_upgrade_all")).failed:
+      rc |=  self.FAILED
+    else:
+      self.log(Logger.SUCCESS, "Successfully updated the Python packages.")
+
+    return rc
+
+  def enable_automatic_upgrades(self):
+    if self.execute("apt", "install", "-y" ,"unattended-upgrades").failed:
       return self.FAILED
 
     for config_file in [Unattended_upgrades.Unattended_upgrades, Unattended_upgrades.Auto_upgrades]:
@@ -38,7 +115,7 @@ class System(Command):
         return self.FAILED
 
     self.log(Logger.SUCCESS, "Successfully configured automatic system upgrades.")
-    return rc
+    return self.SUCCESS
 
 
 
@@ -59,19 +136,17 @@ class System(Command):
     if args.all:
       rc |= self.install_or_update_all()
 
-    elif args.nginx or args.mysql or args.viva_framework or args.viva_website or args.system_update or args.enable_automatic_upgrades:
-      if args.nginx:
-        rc |= self.install_or_update_nginx()
-      if args.mysql:
-        rc |= self.install_or_update_mysql()
+    elif args.LEMP or args.viva_framework or args.viva_website or args.update_packages or args.enable_automatic_upgrades:
+      if args.LEMP:
+        rc |= self.install_or_update_LEMP(args)
       if args.viva_framework:
         rc |= self.install_or_update_viva_framework()
       if args.viva_website:
         rc |= self.install_or_update_viva_website()
-      if args.nginx:
-        rc |= self.system_update()
+      if args.update_packages:
+        rc |= self.update_packages()
       if args.enable_automatic_upgrades:
-        rc |= self.automatic_upgrades()
+        rc |= self.enable_automatic_upgrades()
 
     else:
       self.log(Logger.WARNING, "No arguments were provided.", write_logs=False)
@@ -86,10 +161,9 @@ class System(Command):
 
     parser.add_argument("--all", action="store_true", help="Install and/or update viva and the system completely")
 
-    parser.add_argument("--nginx", action="store_true", help="Install and/or update NGINX")
-    parser.add_argument("--mysql", action="store_true", help="Install and/or update MySQL")
+    parser.add_argument("--LEMP", action="store_true", help="Install and/or update NginX, MySQL and PHP")
     parser.add_argument("--viva-framework", action="store_true", help="Update the viva command")
     parser.add_argument("--viva-website", action="store_true", help="Install and/or update the viva website pages")
 
-    parser.add_argument("--system-update", action="store_true", help="Update the OS packages and python packages")
+    parser.add_argument("--update-packages", action="store_true", help="Update the OS packages and python packages")
     parser.add_argument("--enable-automatic-upgrades", action="store_true", help="Enable automatic system upgrades")
